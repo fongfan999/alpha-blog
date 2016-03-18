f4f9f0dce7c36a242bfd3a1caa90352f6adc2fc3
Phong Phan <fongfan999@gmail.com>
2016-03-18 20:16:29 +0700

    Show user and gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def new
 		@user = User.new
 	end
@@ -14,11 +16,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:success] = "Your account was updated successfully"
 			redirect_to articles_path
@@ -27,8 +27,15 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
 	private
 		def user_params
 			params.require(:user).permit(:username, :email, :password)
+		end
+
+		def set_article
+			@user = User.find(params[:id])
 		end
 end
