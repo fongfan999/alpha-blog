49531f2779b5ec08b615cffdff4ee2fcdee52950
Phong Phan <fongfan999@gmail.com>
2016-03-18 15:08:33 +0700

    Add edit, update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   resources :articles
 
   get "signup", to: "users#new"
-  post "users", to: "users#create"
-  # resources :users, except: [:new]
+  #post "users", to: "users#create"
+  resources :users, except: [:new]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
