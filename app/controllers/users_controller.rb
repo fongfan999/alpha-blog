9281a60b5d9e8e9a45dc375cbfad783f319278a4
Phong Phan <fongfan999@gmail.com>
2016-03-20 10:49:26 +0700

    Login after sign up automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome #{@user.username} to the Alpha blog"
+			session[:user_id] = @user.id
 			redirect_to articles_path
 		else
 			render "new"
